a2d11d09246dcb9315f38d7c39d64fcbb26f2d3e
BekturMuratov <Muratov225@gmail.com>
2021-12-28 13:30:43 +0600

    got acquainted with the basic basics of typescript

--- dist/index.js
+++ dist/index.js
@@ -24,8 +24,67 @@ let num = 22;
 console.log(num);
 var Direction;
 (function (Direction) {
-    Direction[Direction["Up"] = 0] = "Up";
-    Direction[Direction["Down"] = 1] = "Down";
-    Direction[Direction["Left"] = 2] = "Left";
-    Direction[Direction["Right"] = 3] = "Right";
+    Direction[Direction["Up"] = 1] = "Up";
+    Direction[Direction["Down"] = 2] = "Down";
+    Direction[Direction["Left"] = 3] = "Left";
+    Direction[Direction["Right"] = 4] = "Right";
 })(Direction || (Direction = {}));
+console.log(Direction.Left);
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "Up";
+    Direction2["Down"] = "Down";
+    Direction2["Left"] = "Left";
+    Direction2["Right"] = "Right";
+})(Direction2 || (Direction2 = {}));
+console.log(Direction2.Right);
+const user = {
+    id: 1,
+    name: 'John'
+};
+console.log(user.name);
+let cid = 1;
+let customerId = cid;
+console.log(customerId);
+function addNum(x, y) {
+    return x + y;
+}
+console.log(addNum(1, 2));
+function log(message) {
+    return console.log(message);
+}
+const user1 = {
+    id: 1,
+    name: 'Bob',
+};
+console.log(user1);
+const p1 = 1;
+const add = (x, y) => x + y;
+const sub = (x, y) => x - y;
+class Person {
+    constructor(id, name) {
+        this.id = id,
+            this.name = name;
+    }
+    register() {
+        return `${this.name} is now registered`;
+    }
+}
+const brad = new Person(1, 'Brad Traversy');
+const mike = new Person(2, 'Mike Jordan');
+console.log(brad.id = 5);
+console.log(brad, mike);
+console.log(brad.register());
+class Employee extends Person {
+    constructor(id, name, positiion) {
+        super(id, name);
+        this.positiion = positiion;
+    }
+}
+const emp = new Employee(3, 'Henry Cavil', 'actor');
+console.log(emp.register());
+function getArray(items) {
+    return new Array().concat(items);
+}
+let numArray = getArray([1, 2, 3, 4]);
+let strArray = getArray(['brad', 'mike', 'shawn']);
--- src/index.ts
+++ src/index.ts
@@ -36,8 +36,132 @@ console.log(num);
 
 //Enum
 enum Direction {
-    Up,
+    Up = 1,
     Down,
     Left,
     Right,
 }
+
+console.log(Direction.Left);
+
+enum Direction2 {
+    Up = 'Up',
+    Down = 'Down',
+    Left = 'Left',
+    Right = 'Right',
+}
+
+console.log(Direction2.Right);
+
+
+//Objects
+type User ={
+    id:number,
+    name:string,
+}
+
+const user:User = {
+    id:1, 
+    name:'John'
+}
+
+console.log(user.name)
+
+
+//Type assertion
+let cid: any = 1;
+// let customerId = <number>cid;
+let customerId =cid as number;
+console.log(customerId);
+
+
+//Functions 
+
+function addNum(x:number,y:number){
+    return x + y
+}
+
+console.log(addNum(1,2))
+
+
+function log(message: string | number): void{
+    return console.log(message);
+}
+
+// Interfaces
+interface UserInterface {
+    readonly id:number,
+    name: string ,
+    age?:number, 
+}
+
+
+const user1: UserInterface = {
+    id: 1,
+    name: 'Bob',
+}
+
+console.log(user1)
+
+type Point = number | string
+const p1:Point = 1
+
+interface MathFunc {
+    (x:number, y: number):number
+}
+
+const add:MathFunc = (x:number, y: number): number => x + y;
+const sub:MathFunc = (x:number, y: number): number => x - y;
+
+interface PersonInterface {
+    id:number,
+    name: string , 
+    register():string
+}
+
+//Classes
+class Person implements PersonInterface{
+    id:number
+    name: string
+
+    constructor(id:number, name:string){
+       this.id = id,
+       this.name = name
+    }
+
+    register(){
+        return `${this.name} is now registered`
+    }
+}
+
+
+const brad = new Person(1,'Brad Traversy');
+const mike = new Person(2,'Mike Jordan');
+
+console.log(brad.id = 5)
+console.log(brad, mike)
+
+console.log(brad.register())
+
+//Subclasses
+class Employee extends Person{
+    positiion:string
+
+    constructor(id:number, name:string, positiion:string){
+       super(id,name,)
+       this.positiion = positiion
+    }
+}
+
+const emp = new Employee(3, 'Henry Cavil', 'actor')
+console.log(emp.register())
+
+
+//Generics
+function getArray<T>(items:T[]):T[]{
+    return new Array().concat(items)
+}
+
+let numArray = getArray<number>([1,2,3,4])
+let strArray = getArray<string>(['brad','mike','shawn'])
+
